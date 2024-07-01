79c7cc03bdc8e973d1f97d06224affc65ef2bc4c
suda <fujisu71@gmail.com>
2024-07-02 01:27:02 +0900

    移動をX軸に限定

--- Assets/Scripts/CharacterUnitController.cs
+++ Assets/Scripts/CharacterUnitController.cs
@@ -20,38 +20,15 @@ public class CharacterUnitController : MonoBehaviour
         _animator = GetComponent<Animator>();
 
         _playerRotation = _transform.rotation;
+
+        _animator.SetBool("walking", true);
     }
 
     void FixedUpdate()
     {
-        // 移動処理
         float horizontal = Input.GetAxis("Horizontal");
-        float vertical = Input.GetAxis("Vertical");
-        Vector3 aim = new Vector3(horizontal, 0, vertical).normalized;
-        if (aim.magnitude > 0.5f)
-        {
-            _playerRotation = Quaternion.LookRotation(aim, Vector3.up);
-        }
-        // 目標の回転角度を計算
-        Quaternion deltaRotation = Quaternion.Inverse(_transform.rotation) * _playerRotation;
-        // 回転速度を計算
-        float maxDegreesDelta = _maxRotationSpeed * Time.deltaTime;
-        // 目標の回転角度を回転速度で制限
-        Quaternion limitedRotation = Quaternion.RotateTowards(Quaternion.identity, deltaRotation, maxDegreesDelta);
-        // 1フレームごとの回転量を計算
-        Quaternion rotationThisFrame = _transform.rotation * limitedRotation;
-        // 回転を適用
-        _transform.rotation = rotationThisFrame;
-
-        Vector3 velocity = new Vector3(horizontal, _rigidbody.velocity.y, vertical).normalized;
-        if (velocity.magnitude > 0.1f)
-        {
-            _animator.SetBool("walking", true);
-        }
-        else
-        {
-            _animator.SetBool("walking", false);
-        }
+        Vector3 aim = new Vector3(horizontal, 0, 0).normalized;
+        Vector3 velocity = new Vector3(horizontal, _rigidbody.velocity.y, 0).normalized;
         _rigidbody.velocity = velocity * _speed;
     }
 }
